8c25c2f9b78c71b11416e92968b6b739bc61b511
moonelysian <dandyblack@naver.com>
2022-06-23 13:55:18 +0900

    fix: indicator 클릭해서 페이지 이동 기능 추가

--- src/js/index.js
+++ src/js/index.js
@@ -2,18 +2,44 @@ import '../css/init.css';
 import '../css/variables.css';
 import '../css/style.css';
 
-const pageSize = document.querySelector('.page').clientHeight - 10;
-const pageIndex = document.getElementsByClassName('index-icon');
+const pageHight = document.querySelector('.page').clientHeight;
+const triggerSize = pageHight - 5;
+const pageIndicator = document.getElementsByClassName('index-icon');
+const indicatorSection = document.querySelector('.index-section');
 
-let beforeScroll = 0;
+let startPoint = 0;
 
-const handlePageIndex = currentScrollY => {
-  const afterScroll = parseInt(currentScrollY / pageSize);
-  if (beforeScroll !== afterScroll) {
-    pageIndex[beforeScroll].classList.remove('active');
-    pageIndex[afterScroll].classList.add('active');
-    beforeScroll = afterScroll;
-  }
+const handleScroll = currentScrollY => {
+  const destination =
+    parseInt(currentScrollY / triggerSize) % pageIndicator.length;
+  handleIndicator(destination);
+  return;
 };
 
-window.addEventListener('scroll', () => handlePageIndex(window.scrollY));
+const handleClick = dataset => {
+  if (!dataset) {
+    return;
+  }
+  const clickedIndex = dataset - 1;
+  if (clickedIndex !== startPoint) {
+    window.scroll({ top: pageHight * clickedIndex, behavior: 'smooth' });
+    handleIndicator(clickedIndex);
+    return;
+  }
+  return;
+};
+
+const handleIndicator = destination => {
+  if (startPoint !== destination) {
+    pageIndicator[startPoint].classList.remove('active');
+    pageIndicator[destination].classList.add('active');
+    startPoint = destination;
+    return;
+  }
+  return;
+};
+
+window.addEventListener('wheel', () => handleScroll(window.scrollY));
+indicatorSection.addEventListener('click', event =>
+  handleClick(Number(event.target.dataset['index'])),
+);
