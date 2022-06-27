ef56c457a8ae82572394ea36e6b8ddc27359cdef
moonelysian <dandyblack@naver.com>
2022-06-27 17:02:50 +0900

    add: 콘서트 가기 전 페이지 만드는 중

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,8 @@ const pageHight = document.querySelector('.page').clientHeight;
 const triggerSize = pageHight - 5;
 const pageIndicator = document.getElementsByClassName('index-icon');
 const indicatorSection = document.querySelector('.index-section');
+const logoSection = document.querySelector('.logo-section');
+const checklist = document.querySelector('.checklist');
 
 let startPoint = 0;
 
@@ -39,7 +41,25 @@ const handleIndicator = destination => {
   return;
 };
 
+const handleLogo = () => {
+  const clickme = document.querySelector('.clickme');
+  const smiles = document.querySelector('.smile-section');
+  const r = logoSection.querySelector("[data-text='r']");
+  const l = logoSection.querySelector("[data-text='l']");
+  smiles.classList.replace('hidden', 'appear');
+  r.classList.add('hidden');
+  clickme.classList.add('hidden');
+  l.classList.replace('hidden', 'appear');
+};
+
+const handleChecklist = target => {
+  const icon = target.closest('.checklist-item').querySelector('.check-icon');
+  icon.classList.add('checked');
+};
+
 window.addEventListener('wheel', () => handleScroll(window.scrollY));
 indicatorSection.addEventListener('click', event =>
   handleClick(Number(event.target.dataset['index'])),
 );
+logoSection.addEventListener('click', handleLogo);
+checklist.addEventListener('click', e => handleChecklist(e.target));
